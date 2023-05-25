6b35007234dc3f5ea6640acbd2e5a181fbe4fb68
QuantumV <zonictheyozh@gmail.com>
2023-05-25 17:37:18 +0300

    v0.0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class PeppinoModCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.compbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tipLabel = new System.Windows.Forms.Label();
+            this.objTextBox = new System.Windows.Forms.TextBox();
+            this.objlabel = new System.Windows.Forms.Label();
+            this.varTextBoxName = new System.Windows.Forms.TextBox();
+            this.relCheckBox = new System.Windows.Forms.CheckBox();
+            this.varTextBoxVal = new System.Windows.Forms.TextBox();
+            this.varlabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addComp = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // compbutton
+            // 
+            this.compbutton.Location = new System.Drawing.Point(206, 350);
+            this.compbutton.Name = "compbutton";
+            this.compbutton.Size = new System.Drawing.Size(243, 88);
+            this.compbutton.TabIndex = 0;
+            this.compbutton.Text = "Compile";
+            this.compbutton.UseVisualStyleBackColor = true;
+            this.compbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(91, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // tipLabel
+            // 
+            this.tipLabel.AutoSize = true;
+            this.tipLabel.Location = new System.Drawing.Point(523, 28);
+            this.tipLabel.Name = "tipLabel";
+            this.tipLabel.Size = new System.Drawing.Size(28, 13);
+            this.tipLabel.TabIndex = 1;
+            this.tipLabel.Text = "Tip: ";
+            // 
+            // objTextBox
+            // 
+            this.objTextBox.Location = new System.Drawing.Point(94, 67);
+            this.objTextBox.Name = "objTextBox";
+            this.objTextBox.Size = new System.Drawing.Size(100, 20);
+            this.objTextBox.TabIndex = 2;
+            this.objTextBox.Text = "obj_noiseboss";
+            this.objTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // objlabel
+            // 
+            this.objlabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.objlabel.AutoSize = true;
+            this.objlabel.Location = new System.Drawing.Point(118, 51);
+            this.objlabel.Name = "objlabel";
+            this.objlabel.Size = new System.Drawing.Size(38, 13);
+            this.objlabel.TabIndex = 3;
+            this.objlabel.Text = "Object";
+            // 
+            // varTextBoxName
+            // 
+            this.varTextBoxName.Location = new System.Drawing.Point(94, 119);
+            this.varTextBoxName.Name = "varTextBoxName";
+            this.varTextBoxName.Size = new System.Drawing.Size(100, 20);
+            this.varTextBoxName.TabIndex = 4;
+            this.varTextBoxName.Text = "paletteselect";
+            // 
+            // relCheckBox
+            // 
+            this.relCheckBox.AutoSize = true;
+            this.relCheckBox.Location = new System.Drawing.Point(94, 171);
+            this.relCheckBox.Name = "relCheckBox";
+            this.relCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.relCheckBox.TabIndex = 5;
+            this.relCheckBox.Text = "Relative";
+            this.relCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // varTextBoxVal
+            // 
+            this.varTextBoxVal.Location = new System.Drawing.Point(94, 145);
+            this.varTextBoxVal.Name = "varTextBoxVal";
+            this.varTextBoxVal.Size = new System.Drawing.Size(100, 20);
+            this.varTextBoxVal.TabIndex = 6;
+            this.varTextBoxVal.Text = "3";
+            // 
+            // varlabel
+            // 
+            this.varlabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.varlabel.AutoSize = true;
+            this.varlabel.Location = new System.Drawing.Point(118, 103);
+            this.varlabel.Name = "varlabel";
+            this.varlabel.Size = new System.Drawing.Size(45, 13);
+            this.varlabel.TabIndex = 7;
+            this.varlabel.Text = "Variable";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Updates not found";
+            // 
+            // addComp
+            // 
+            this.addComp.Location = new System.Drawing.Point(477, 350);
+            this.addComp.Name = "addComp";
+            this.addComp.Size = new System.Drawing.Size(166, 88);
+            this.addComp.TabIndex = 9;
+            this.addComp.Text = "Add Component";
+            this.addComp.UseVisualStyleBackColor = true;
+            this.addComp.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(504, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "var";
+            // 
+            // PeppinoModCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addComp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tipLabel);
+            /*this.Controls.Add(this.varlabel);
+            this.Controls.Add(this.varTextBoxVal);
+            this.Controls.Add(this.relCheckBox);
+            this.Controls.Add(this.varTextBoxName);
+            this.Controls.Add(this.objlabel);
+            this.Controls.Add(this.objTextBox);
+            */
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.compbutton);
+            this.Name = "PeppinoModCreator";
+            this.Text = "Peppino\'s Mod Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button compbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tipLabel;
+        private System.Windows.Forms.TextBox objTextBox;
+        private System.Windows.Forms.Label objlabel;
+        private System.Windows.Forms.TextBox varTextBoxName;
+        private System.Windows.Forms.CheckBox relCheckBox;
+        private System.Windows.Forms.TextBox varTextBoxVal;
+        private System.Windows.Forms.Label varlabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addComp;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class PeppinoModCreator : Form
+    {
+        List<string> lsCode = new List<string>();
+        
+        public PeppinoModCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormClosing += QuitAPP;
+            string[] tipArray = { "I'm a Creeper\nMinecrafts Grim Reaper", "PEPPINO YOURE MINE!\nYOUVE TAKEN FAR TOO MUCH\nTIME FOR A SIMPLE JOG TO\nTHE EXIT DOOR", "Fester out all of your\ndoubt", "And this is story all\nabout how my life got\nflipped upside down", "WHEN THE", "Did you know that you\n could make uhh uhhh" };
+            Random r = new Random();
+            int rInt = r.Next(0, tipArray.Length);
+            this.tipLabel.Text = "Tip: " + tipArray[rInt];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!relCheckBox.Checked) 
+            {
+            lsCode.Add(varTextBoxName.Text + " = " + varTextBoxVal.Text);
+            }
+            else
+            {
+                lsCode.Add(varTextBoxName.Text + " += " + varTextBoxVal.Text);
+            }
+            var formPopup = new Form();
+            formPopup.Show(this);
+            formPopup.Name = "Add this to " + objTextBox.Text;
+            formPopup.Text = "Add this to " + objTextBox.Text;
+            TextBox code = new TextBox()
+            {
+                Text = string.Join("\n", lsCode.ToArray()),
+                ReadOnly = true,
+                BorderStyle = 0,
+                BackColor = this.BackColor,
+                TabStop = false,
+                Multiline = true,
+                Location = new Point(10, 10),
+                TabIndex = 10,
+                Size = new System.Drawing.Size(99, 500)
+        };
+
+            formPopup.Controls.Add(code);
+            //lsCode.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        Form formPopup_Comp = new Form();
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!formPopup_Comp.Visible)
+            {
+                formPopup_Comp.Show(this);
+                formPopup_Comp.Controls.Add(this.varlabel);
+                formPopup_Comp.Controls.Add(this.varTextBoxVal);
+                formPopup_Comp.Controls.Add(this.relCheckBox);
+                formPopup_Comp.Controls.Add(this.varTextBoxName);
+                formPopup_Comp.Controls.Add(this.objlabel);
+                formPopup_Comp.Controls.Add(this.objTextBox);
+                formPopup_Comp.FormClosing += DontLiquidate;
+            }
+
+        }
+        
+        // Use this event handler for the FormClosing event.
+        private void DontLiquidate(object sender, FormClosingEventArgs e)
+        {
+            formPopup_Comp.Hide();
+            e.Cancel = true; // this cancels the close event.
+        }
+        private void QuitAPP(object sender, FormClosingEventArgs e)
+        {
+            if (formPopup_Comp != null) { 
+            formPopup_Comp.FormClosing -= DontLiquidate;
+            }
+            e.Cancel = false;
+        }
+    }
+}
