733feadbfd4537a54ac08d6324c3ba4255a13187
QuantumV <zonictheyozh@gmail.com>
2023-05-26 12:17:41 +0300

    dont ask just delete it its a error

---
+++ Addto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Addto : Form
+	{
+		public Addto()
+		{
+			InitializeComponent();
+		}
+		Point lastPoint;
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Addto_MouseDown(object sender, MouseEventArgs e)
+		{
+			lastPoint = new Point(e.X, e.Y);
+		}
+
+		private void Addto_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				this.Left += e.X - lastPoint.X;
+				this.Top += e.Y - lastPoint.Y;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+        }
+	}
+}
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,16 +40,26 @@
             this.label2 = new System.Windows.Forms.Label();
             this.addComp = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // compbutton
             // 
+            this.compbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.compbutton.FlatAppearance.BorderSize = 0;
+            this.compbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.compbutton.Font = new System.Drawing.Font("Pepperoni", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compbutton.ForeColor = System.Drawing.SystemColors.Control;
             this.compbutton.Location = new System.Drawing.Point(206, 350);
             this.compbutton.Name = "compbutton";
             this.compbutton.Size = new System.Drawing.Size(243, 88);
             this.compbutton.TabIndex = 0;
             this.compbutton.Text = "Compile";
-            this.compbutton.UseVisualStyleBackColor = true;
+            this.compbutton.UseVisualStyleBackColor = false;
             this.compbutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
@@ -62,9 +72,11 @@
             // tipLabel
             // 
             this.tipLabel.AutoSize = true;
-            this.tipLabel.Location = new System.Drawing.Point(523, 28);
+            this.tipLabel.Font = new System.Drawing.Font("Pepperoni", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.tipLabel.Location = new System.Drawing.Point(658, 52);
             this.tipLabel.Name = "tipLabel";
-            this.tipLabel.Size = new System.Drawing.Size(28, 13);
+            this.tipLabel.Size = new System.Drawing.Size(39, 16);
             this.tipLabel.TabIndex = 1;
             this.tipLabel.Text = "Tip: ";
             // 
@@ -130,51 +142,114 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Font = new System.Drawing.Font("Pepperoni", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(12, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.Size = new System.Drawing.Size(136, 16);
             this.label2.TabIndex = 8;
             this.label2.Text = "Updates not found";
             // 
             // addComp
             // 
+            this.addComp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.addComp.FlatAppearance.BorderSize = 0;
+            this.addComp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addComp.Font = new System.Drawing.Font("Pepperoni", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addComp.ForeColor = System.Drawing.SystemColors.Control;
             this.addComp.Location = new System.Drawing.Point(477, 350);
             this.addComp.Name = "addComp";
             this.addComp.Size = new System.Drawing.Size(166, 88);
             this.addComp.TabIndex = 9;
             this.addComp.Text = "Add Component";
-            this.addComp.UseVisualStyleBackColor = true;
+            this.addComp.UseVisualStyleBackColor = false;
             this.addComp.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Pepperoni", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Control;
             this.textBox1.Location = new System.Drawing.Point(504, 324);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 16);
             this.textBox1.TabIndex = 10;
             this.textBox1.Text = "var";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(-8, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(908, 50);
+            this.panel1.TabIndex = 11;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Pepperoni", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(819, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Pepperoni", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(858, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Pepperoni", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(19, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(294, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Peppino\'s Mod Creator V0.0";
             // 
             // PeppinoModCreator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(655, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(895, 446);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.addComp);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tipLabel);
-            /*this.Controls.Add(this.varlabel);
-            this.Controls.Add(this.varTextBoxVal);
-            this.Controls.Add(this.relCheckBox);
-            this.Controls.Add(this.varTextBoxName);
-            this.Controls.Add(this.objlabel);
-            this.Controls.Add(this.objTextBox);
-            */
             this.Controls.Add(this.label1);
             this.Controls.Add(this.compbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PeppinoModCreator";
             this.Text = "Peppino\'s Mod Creator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,6 +269,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button addComp;
         private System.Windows.Forms.TextBox textBox1;
-    }
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+	}
 }
 
